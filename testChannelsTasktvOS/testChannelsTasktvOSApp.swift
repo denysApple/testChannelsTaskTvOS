08390b2df7eece8e4ba66187bde118870d0a2c34
Denys Havryliak <den4iklvivua@gmail.com>
2023-06-10 17:12:14 +0300

    v. 0

---
+++ testChannelsTasktvOS/testChannelsTasktvOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  testChannelsTasktvOSApp.swift
+//  testChannelsTasktvOS
+//
+//  Created by Denys on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct testChannelsTasktvOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
